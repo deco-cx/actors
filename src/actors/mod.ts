93d22dca76f0e31df13d20dc3d90bacdd210201c
Marcos Candeia <marrcooos@gmail.com>
2024-12-02 19:33:11 -0300

    Export actor Id
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- src/actors/mod.ts
+++ src/actors/mod.ts
@@ -7,3 +7,5 @@ export { ActorRuntime } from "./runtime.ts";
 export type { ActorConstructor } from "./runtime.ts";
 export { ActorState } from "./state.ts";
 export { type ActorStorage } from "./storage.ts";
+export { actorId } from "./util/id.ts";
+
