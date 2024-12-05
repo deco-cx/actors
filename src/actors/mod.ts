825441a557d1b373f160f3f6e9465b22eaf1e4db
Marcos Candeia <marrcooos@gmail.com>
2024-12-05 16:29:08 -0300

    Revert "Revert "Rename proxy to stub (#19)" (#20)" (#21)
    This reverts commit e2e7c392c4c2541451daf4f381ef9577b8e76329.

--- src/actors/mod.ts
+++ src/actors/mod.ts
@@ -2,9 +2,9 @@
 export interface Actor {
 }
 
-export type { ActorProxy } from "./proxyutil.ts";
 export { ActorRuntime } from "./runtime.ts";
 export type { ActorConstructor } from "./runtime.ts";
 export { ActorState } from "./state.ts";
 export { type ActorStorage } from "./storage.ts";
+export type { ActorProxy, ActorProxy as ActorStub } from "./stubutil.ts";
 export { actorId } from "./util/id.ts";
