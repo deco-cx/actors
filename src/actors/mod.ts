8b67d8ab0a29760dab380794f3d2ae262487d5a0
Marcos Candeia <marrcooos@gmail.com>
2025-01-11 13:35:34 -0300

    Backward compat with old name
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- src/actors/mod.ts
+++ src/actors/mod.ts
@@ -1,6 +1,8 @@
 // deno-lint-ignore no-empty-interface
 export interface Actor {
 }
+// Backwards compatibility for the old name
+export { StdActorRuntime as ActorRuntime } from "./runtime.ts";
 
 export { StdActorRuntime } from "./runtime.ts";
 export type { ActorConstructor } from "./runtime.ts";
@@ -8,3 +10,4 @@ export { ActorState } from "./state.ts";
 export { type ActorStorage } from "./storage.ts";
 export type { ActorProxy, ActorProxy as ActorStub } from "./stubutil.ts";
 export { actorId } from "./util/id.ts";
+
